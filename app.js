da0c5d8a988d0bdba3ed0179a7e95e05cbc1ee1e
BlueZoft <akilaariyathilaka@gmail.com>
2024-03-05 11:26:21 +0000

    full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Take it all easy but steady'));
+app.get('/', (req, res) => res.send('With perseverance, good things happen'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
